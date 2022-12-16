dd521dd432be8a8d22ef417b2683bfdf97044f63
🎈 <numofran6@gmail.com>
2022-12-16 15:05:29 +0000

    0/5

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const messageRoutes = require('./routes/messageRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', req.header('origin'));
+// app.use(function (req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', req.header('origin'));
 
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'POST, OPTIONS, GET, PUT, UPDATE, DELETE'
-	);
+// 	res.setHeader(
+// 		'Access-Control-Allow-Methods',
+// 		'POST, OPTIONS, GET, PUT, UPDATE, DELETE'
+// 	);
 
-	// Request headers you wish to allow
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'X-Requested-With,content-type'
-	);
+// 	// Request headers you wish to allow
+// 	res.setHeader(
+// 		'Access-Control-Allow-Headers',
+// 		'X-Requested-With,content-type'
+// 	);
 
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	// Set to true if you need the website to include cookies in the requests sent
+// 	// to the API (e.g. in case you use sessions)
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
 
-	// Pass to next layer of middleware
-	next();
-});
+// 	// Pass to next layer of middleware
+// 	next();
+// });
 
 app.use('/api/auth/', userRoutes);
 app.use('/api/messages/', messageRoutes);
